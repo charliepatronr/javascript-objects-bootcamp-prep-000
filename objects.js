96680226e7cf1ee0092883f52769d33798161aa9
Juan Carlos Patron <charliepatronr@gmail.com>
2019-12-31 00:02:15 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ var playlist = { name: "The Killers", song : "Shadowplay" };
 
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist.name[artistName];
-  playlist.song[songTitle];
-  return playlist;
+  playlist.name = artistName;
+   playlist.song = songTitle;
+   return playlist;
 }
